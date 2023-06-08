a630d79b67da29186ca04270ab556118ba21402d
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2023-06-08 09:37:39 +0100

    Add firebase.

--- src/main.js
+++ src/main.js
@@ -5,14 +5,16 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createMetaManager } from 'vue-meta'
 
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import firebase from '@/firebase'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(createMetaManager())
+app.use(firebase)
 
 await router.isReady()
 
